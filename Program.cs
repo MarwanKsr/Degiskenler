4c545934741123a0ef42e071d8baec2869424b3f
Marwan_Kaseer <Marwan.kaseer1997@gmail.com>
2022-02-07 20:06:25 +0300

    program is done

---
+++ Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace Degisken
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            short sh1 = -5;
+            ushort sh2 = 5;
+            byte b1 = 6;
+            sbyte b2 = -5;
+            int i1 = -7;
+            uint i2 = 7;
+            long l1 = -8;
+            ulong l2 = 8;
+
+            Console.WriteLine(sh1);
+            Console.WriteLine(sh2);
+            Console.WriteLine(b1);
+            Console.WriteLine(b2);
+            Console.WriteLine(i1);
+            Console.WriteLine(i2);
+            Console.WriteLine(l1);
+            Console.WriteLine(l2);
+
+            string adı = "Marwan";
+            string soyad = "Kaseer";
+            Console.WriteLine(adı + " " + soyad);
+
+            float j1 = 0.50f;
+            double d1 = 2.5;
+            decimal d2 = 3.5m;
+
+            Console.WriteLine(j1);
+            Console.WriteLine(d1);
+            Console.WriteLine(d2);
+
+            Console.WriteLine(DateTime.Now);
+
+            string str =Convert.ToString(i1);
+            string str1 = Convert.ToString(d1);
+
+            Console.WriteLine(DateTime.Now.ToString("dd.MM.yyyy"));
+
+
+
+
+
+
+
+        }
+    }
+}
